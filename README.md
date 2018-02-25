d02fa0959e248af8532a78c197fc2acf01356188
ShahrukhMushtaq <ShahrukhMushtaq23@gmail.com>
2018-02-26 01:45:34 +0500

    Completed

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CRUD
+![alt text](Capture.png)
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.8.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular CRUD APPLICATION';
+students = [
+{name:"Arslan",seat:"EP-1449110",class:"BSCS",year:"4rth"},
+{name:"Awais",seat:"EP-1449021",class:"BSCS",year:"4rth"},
+{name:"Hamza",seat:"EP-1449032",class:"BSCS",year:"4rth"},
+{name:"Hidayat",seat:"EP-1449037",class:"BSCS",year:"4rth"},
+{name:"Shahrukh",seat:"EP-1449099",class:"BSCS",year:"4rth"},
+
+];
+
+stud:any={};
+stud1:any={};
+
+addStudents(){
+this.students.push(this.stud);
+this.stud={};
+}
+
+deleteStudents(i){
+  console.log(i);
+  this.students.splice(i,1);
+}
+myVal;
+editStudents(j){
+
+  this.stud1.name = this.students[j].name;
+  this.stud1.seat = this.students[j].seat;
+  this.stud1.class = this.students[j].class;
+  this.stud1.year = this.students[j].year;
+  this.myVal = j;
+}
+
+updateStudents(i){
+  let k = this.myVal;
+  for(let i=0; i<this.students.length; i++)
+  {
+    if(i==k){
+      this.students[i] = this.stud1;
+      this.stud1 = {};
+    }
+  }
+}
+
 }
